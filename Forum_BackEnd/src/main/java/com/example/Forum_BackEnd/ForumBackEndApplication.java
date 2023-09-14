1b130405e2dbe25da70b2411ef9250ebebf57ea7
Youness-Madouni <ymadouni3@gmail.com>
2023-09-14 20:49:23 +0100

    first commit

---
+++ Forum_BackEnd/src/main/java/com/example/Forum_BackEnd/ForumBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.Forum_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumBackEndApplication.class, args);
+	}
+
+}
